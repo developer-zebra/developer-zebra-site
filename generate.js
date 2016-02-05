5b055f3ef382e256d6e64172dbf6fc34edfc7e81
cfh463 <rob.galvin@zebra.com>
2016-02-05 16:00:41 -0500

    added csp collection to generate.js

--- generate.js
+++ generate.js
@@ -206,7 +206,11 @@ var sitebuild = Metalsmith(__dirname)
         },
         md: {
             pattern: '/**/**.md'
-        }
+        },
+        csp: {
+            pattern: 'mx/**/*.csp',
+            sortBy: 'order'
+        },
 
     }))
     .use(findLayout({

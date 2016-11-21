fcfc1a2c5aa90d96a5d185cb88c9ec4a17dca9e8
cfh463 <rob.galvin@zebra.com>
2016-11-21 14:24:38 -0500

    updated mx matrix to have 6.1 drop down

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ var index_generate = function(folder){
 
 }
 
-      index_generate("emdk-for-android/6-0");
+      index_generate("enterprise-keyboard/1-2");
+      index_generate("enterprise-keyboard/1-1");
       
 

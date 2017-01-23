1405acd83c494a75dde4dc86a6ace8b70c37e839
Edward Correia <edward.correia@zebra.com>
2017-01-23 14:19:57 -0500

    [TUT-14471] EKB 1.3 update build script

--- build.js
+++ build.js
@@ -640,6 +640,9 @@ var sitebuild = Metalsmith(__dirname)
        .use(foldermenu({
         folder: 'enterprise-keyboard/1-2/'
     }))
+      .use(foldermenu({
+        folder: 'enterprise-keyboard/1-3/'
+    }))
     .use(showdown({literalMidWordUnderscores:true,ghCodeBlocks:false,smartIndentationFix:true}))
     .use(snippet({
       maxLength: 250,

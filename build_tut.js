498de99a8e7a4794dfe0e97f6e93a2b8209facd4
Edward Correia <edward.correia@zebra.com>
2019-11-04 08:09:06 -0500

    [TUT-33932] SN 4.0 update About, build script

--- build_tut.js
+++ build_tut.js
@@ -1472,6 +1472,9 @@ var sitebuild = Metalsmith(__dirname)
         folder: 'stagenow/3-4/'
     }))
     .use(foldermenu({
+        folder: 'stagenow/4-0/'
+    }))
+    .use(foldermenu({
         folder: 'emmtk/2-0/'
     }))
 
